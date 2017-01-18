3596199b84bbc921bb175d7ee5d19fd015b5e1c8
R B Gwozdz <richard.gwozdz@gmail.com>
2017-01-18 09:00:05 -0500

    Commit

--- spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/gwozdz/controllers/UserController.java
+++ spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/gwozdz/controllers/UserController.java
@@ -1,41 +1,51 @@
 package com.gwozdz.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gwozdz.models.User;
 import com.gwozdz.models.UserDao;
 
-@ControllerAdvice
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * A class to test interactions with the MySQL database using the UserDao class.
+ *
+ * @author netgloo
+ */
+@Controller
 public class UserController {
 
-	// Private fields
-
-	@Autowired
-	private UserDao userDao;
-	  
+  // ------------------------
+  // PUBLIC METHODS
+  // ------------------------
+  
   /**
-   * GET /create  --> Create a new user and save it in the database.
+   * /create  --> Create a new user and save it in the database.
+   * 
+   * @param email User's email
+   * @param name User's name
+   * @return A string describing if the user is succesfully created or not.
    */
   @RequestMapping("/create")
   @ResponseBody
   public String create(String email, String name) {
-    String userId = "";
+    User user = null;
     try {
-      User user = new User(email, name);
+      user = new User(email, name);
       userDao.save(user);
-      userId = String.valueOf(user.getId());
     }
     catch (Exception ex) {
       return "Error creating the user: " + ex.toString();
     }
-    return "User succesfully created with id = " + userId;
+    return "User succesfully created! (id = " + user.getId() + ")";
   }
   
   /**
-   * GET /delete  --> Delete the user having the passed id.
+   * /delete  --> Delete the user having the passed id.
+   * 
+   * @param id The id of the user to delete
+   * @return A string describing if the user is succesfully deleted or not.
    */
   @RequestMapping("/delete")
   @ResponseBody
@@ -45,19 +55,21 @@ public class UserController {
       userDao.delete(user);
     }
     catch (Exception ex) {
-      return "Error deleting the user:" + ex.toString();
+      return "Error deleting the user: " + ex.toString();
     }
     return "User succesfully deleted!";
   }
   
   /**
-   * GET /get-by-email  --> Return the id for the user having the passed
-   * email.
+   * /get-by-email  --> Return the id for the user having the passed email.
+   * 
+   * @param email The email to search in the database.
+   * @return The user id or a message error if the user is not found.
    */
   @RequestMapping("/get-by-email")
   @ResponseBody
   public String getByEmail(String email) {
-    String userId = "";
+    String userId;
     try {
       User user = userDao.findByEmail(email);
       userId = String.valueOf(user.getId());
@@ -69,8 +81,13 @@ public class UserController {
   }
   
   /**
-   * GET /update  --> Update the email and the name for the user in the 
-   * database having the passed id.
+   * /update  --> Update the email and the name for the user in the database 
+   * having the passed id.
+   * 
+   * @param id The id for the user to update.
+   * @param email The new email.
+   * @param name The new name.
+   * @return A string describing if the user is succesfully updated or not.
    */
   @RequestMapping("/update")
   @ResponseBody
@@ -87,4 +104,11 @@ public class UserController {
     return "User succesfully updated!";
   }
 
-}
+  // ------------------------
+  // PRIVATE FIELDS
+  // ------------------------
+
+  @Autowired
+  private UserDao userDao;
+  
+} // class UserController
--- spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/gwozdz/models/UserDao.java
+++ spring-boot-mysql-springdatajpa-hibernate/src/main/java/com/gwozdz/models/UserDao.java
@@ -1,13 +1,26 @@
 package com.gwozdz.models;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * A DAO for the entity User is simply created by extending the CrudRepository
+ * interface provided by spring. The following methods are some of the ones
+ * available from such interface: save, delete, deleteAll, findOne and findAll.
+ * The magic is that such methods must not be implemented, and moreover it is
+ * possible create new query methods working only by defining their signature!
+ * 
+ * @author netgloo
+ */
+@Transactional
 public interface UserDao extends CrudRepository<User, Long> {
-	/**
-	   * This method will find an User instance in the database by its email.
-	   * Note that this method is not implemented and its working code will be
-	   * automagically generated from its signature by Spring Data JPA.
-	   */
-	  public User findByEmail(String email);
-	
-}
+
+  /**
+   * Return the user having the passed email or null if no user is found.
+   * 
+   * @param email the user email.
+   */
+  public User findByEmail(String email);
+
+} // class UserDao
